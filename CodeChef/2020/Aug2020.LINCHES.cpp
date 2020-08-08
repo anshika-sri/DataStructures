2ef25daf38599218a17bb3f5bd674a8759e0bec8
anshika-sri <35023358+anshika-sri@users.noreply.github.com>
2020-08-09 04:10:07 +0530

    Create Aug2020.LINCHES.cpp

---
+++ CodeChef/2020/Aug2020.LINCHES.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#define N 1000
+#include <algorithm>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t) {
+	    long long int n, p[N], val, res1 = -1;
+	    cin>>n>>val;
+	    for(int i=0;i<n;i++) {
+          cin>>p[i];
+        }
+        //default is ascending order
+        sort(p, p+n, greater<int>());
+	    for(int i=0;i<n;i++) {
+	        if((val%p[i]) == 0) {
+                res1 = p[i];
+                break;
+            }
+	    }
+	    cout<<res1<<endl;
+        
+	    t--;
+	}
+	return 0;
+}
