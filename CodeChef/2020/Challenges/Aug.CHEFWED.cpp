76b00903a79de5f21a21917f78662f26b4b5c8c3
anshika-sri <35023358+anshika-sri@users.noreply.github.com>
2020-08-26 02:40:51 +0530

    Update Aug.CHEFWED.cpp

--- CodeChef/2020/Challenges/Aug.CHEFWED.cpp
+++ CodeChef/2020/Challenges/Aug.CHEFWED.cpp
@@ -2,35 +2,52 @@
 //none testcases passed
 
 #include <iostream>
-using namespace std;
+#include <bits/stdc++.h>
 #include <algorithm>
 
+using namespace std;
+
+int quarrel(int start, int end, int arr[]) {
+    unordered_map < int, int > mp;
+    int count = 0;
+
+    // Store all the elements in the map with 
+    // their occurrence 
+    for (int i = start; i < end; i++)
+        mp[arr[i]]++;
+
+    // Traverse the map and print all the 
+    // elements with occurrence 1 
+    for (auto it = mp.begin(); it != mp.end(); it++) {
+        if (it - > second == 1)
+            count++;
+    }
+    return end - start + 1 - count;
+}
+
 int main() {
-	// your code goes here
-	
-	int t;
-	cin>>t;
-	while(t) {
-	    int n,k,a[1000], countTable = 1;
-	    cin>>n>>k;
-	    for(int i=0;i<n;i++) {
-	        cin>>a[i];
-	    }
+    int t;
+    cin >> t;
+    while (t > 0) {
+        int n, k, i;
+        cin >> n >> k;
+        int wed[n], dp[n];
+
+        for (int i = 0; i < n; i++) {
+            cin >> wed[i];
+        }
+
+        dp[0] = k;
 	    
-	    sort(a,a+n);
-	    
-	    for(int i=0;i<n;i++) {
-	        if(a[i] == a[i+1]) {
-	            countTable ++;
-	        }
-	    }
-	    if((k*countTable) > (k+countTable)) {
-	        cout<<k+countTable<<endl;
-	    }
-	    else{
-	        cout<<k*countTable<<endl;
-	    }
-	    t--;
-	}
-	return 0;
+        for (i = 1; i < n; i++) {
+            dp[i] = dp[i - 1] + k; //2
+            for (int k1 = i - 1; k1 >= 1; k1--) {
+                dp[i] = min(dp[i], dp[k1 - 1] + k + quarrel(k1, i, wed));
+            }
+        }
+
+        cout << dp[n - 1] << endl;
+        t--;
+    }
+    return 0;
 }
